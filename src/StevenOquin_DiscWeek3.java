65beb5aa6e848c324e386e154eaaf23d49e1d16d
Steven Oquin <steven.beatty11@gmail.com>
2023-01-31 20:30:16 -0700

    change

--- src/StevenOquin_DiscWeek3.java
+++ src/StevenOquin_DiscWeek3.java
@@ -30,6 +30,7 @@ public class StevenOquin_DiscWeek3 {
         } catch(InputMismatchException e){
             System.out.println("Invalid input. Please enter a positive whole number.");
         }
+        System.out.println("please");
         scanint.close();
     }
 }
