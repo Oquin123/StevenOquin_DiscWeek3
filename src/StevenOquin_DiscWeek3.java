61897c3697ed5e990e91ba65f0f19958f3d59800
Steven Oquin <steven.beatty11@gmail.com>
2023-01-31 20:19:20 -0700

    Initial commit

---
+++ src/StevenOquin_DiscWeek3.java
@@ -0,0 +1,35 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class StevenOquin_DiscWeek3 {
+
+    public static void main(String[] args) {
+
+        // Creating a int scanner
+        Scanner scanint = new Scanner(System.in);
+
+        //Prompting user for the following question.
+        System.out.println("How old are you? ");
+
+        //adding a try-catch block to catch the exception and display a message to the user to enter a valid age.
+        try {
+            int Age = scanint.nextInt();
+
+            //Creating if-else statements to decide weather they were born in the 1900's or in the 2000's
+            if (Age > 123)
+                System.out.println("you were born before the 1900's");
+
+            else if(Age >= 23)
+                System.out.println("You were born in the 1900's.");
+
+            else if (Age > 0)
+                System.out.println("You were born in the 2000's.");
+
+            else
+                System.out.println("Please enter your age as a positive number.");
+
+        } catch(InputMismatchException e){
+            System.out.println("Invalid input. Please enter a positive whole number.");
+        }
+        scanint.close();
+    }
+}
